f6f7d957ac250a562ecfa637d8b460180fd6ff2d
surya <suryasuresh9876543@gmail.com>
2021-08-15 15:28:42 +0530

    feat: added input textarea button and out[ut text area

---
+++ app.js
@@ -0,0 +1,4 @@
+var inputTextArea=document.querySelector("#txt-input");
+var outputEl=document.querySelector("#txt-output");
+var translateBtn=document.querySelector("#btn-translate");
+console.log(inputTextArea,outputEl,translateBtn);
